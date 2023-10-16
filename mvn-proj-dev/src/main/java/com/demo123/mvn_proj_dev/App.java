6bf5e77734bb62567e211c8105c1f49fd15cf32f
VenkatKamani225 <136736073+VenkatKamani225@users.noreply.github.com>
2023-10-16 12:47:33 -0400

    Add files via upload

---
+++ mvn-proj-dev/src/main/java/com/demo123/mvn_proj_dev/App.java
@@ -0,0 +1,13 @@
+package com.demo123.mvn_proj_dev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
